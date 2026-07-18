0ce04bdee4cd8ba34eb2dbb3cfdc67164fd491ff
srgranpisito <pintoruben201@gmail.com>
2026-07-18 00:57:02 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
-st.set_page_config(page_title="Calculadora Pro de Mantenciones C&R")
-st.title("🛠️ Calculadora Pro - Mantenciones C&R")
+st.set_page_config(page_title="Calculadora Pro de Mantenciones Beta")
+st.title("🛠️ Calculadora Pro - Mantenciones Beta")
 
 # --- SECCIÓN 1: SERVICIOS ---
 st.header("1. Servicios")
